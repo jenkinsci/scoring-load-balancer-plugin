448bd65ee6e7ca74308da514b75d9c675efb90b9
ikedam <devld@ikedam.jp>
2013-08-28 22:25:38 +0900

    Added tests for NodeLoadScoringRule.

--- src/test/java/jp/ikedam/jenkins/plugins/scoringloadbalancer/rules/NodeLoadScoringRuleJenkinsTest.java
+++ src/test/java/jp/ikedam/jenkins/plugins/scoringloadbalancer/rules/NodeLoadScoringRuleJenkinsTest.java
@@ -157,6 +157,7 @@ public class NodeLoadScoringRuleJenkinsTest
         setScoringRule(new NodeLoadScoringRule(5, 3, 0));
         
         FreeStyleProject testingProject = j.createFreeStyleProject();
+        testingProject.setAssignedLabel(LabelExpression.parseExpression("node"));
         DummySubTask taskOnSameNode = new DummySubTask("Task on same node", testingProject, 1000);
         taskOnSameNode.setSameNodeConstraint(testingProject);
         DummySubTask taskOnAnotherNode1 = new DummySubTask("Task on another node 1", testingProject, 1000);
@@ -176,5 +177,114 @@ public class NodeLoadScoringRuleJenkinsTest
         assertEquals(60, testScoringRule.nodesScoreList.get(0).getScore(node1));
         assertEquals(30, testScoringRule.nodesScoreList.get(1).getScore(node1));
         assertEquals(15, testScoringRule.nodesScoreList.get(2).getScore(node1));
+    }
+    
+    @Test
+    public void testBusyScore() throws Exception
+    {
+        Node node1 = j.createOnlineSlave("node", 3);
+        Node node2 = j.createOnlineSlave("node", 2);
+        
+        setScoringRule(new NodeLoadScoringRule(10, 0, -2));
+        
+        FreeStyleProject testingProject = j.createFreeStyleProject();
+        FreeStyleProject node1Project = j.createFreeStyleProject();
+        node1Project.setAssignedNode(node1);
+        node1Project.getBuildersList().add(new SleepBuilder(60 * 1000));
+        node1Project.setConcurrentBuild(true);
+        
+        FreeStyleProject node2Project = j.createFreeStyleProject();
+        node2Project.setAssignedNode(node2);
+        node2Project.getBuildersList().add(new SleepBuilder(60 * 1000));
+        node2Project.setConcurrentBuild(true);
+        
+        assertEquals(3, node1.toComputer().getExecutors().size());
+        assertEquals(2, node2.toComputer().getExecutors().size());
+        
+        // node1 busy 0
+        // node2 busy 0
+        testScoringRule.clear();
+        j.assertBuildStatusSuccess(testingProject.scheduleBuild2(0).get(BUILD_TIMEOUT, TimeUnit.SECONDS));
+        assertEquals(1, testScoringRule.nodesScoreList.size());
+        assertEquals(0, testScoringRule.nodesScoreList.get(0).getScore(node1));
+        assertEquals(0, testScoringRule.nodesScoreList.get(0).getScore(node2));
+        
+        // build on node1
+        j.startBuild(node1Project, BUILD_TIMEOUT);
+        
+        // node1 busy 1
+        // node2 busy 0
+        testScoringRule.clear();
+        j.assertBuildStatusSuccess(testingProject.scheduleBuild2(0).get(BUILD_TIMEOUT, TimeUnit.SECONDS));
+        assertEquals(1, testScoringRule.nodesScoreList.size());
+        assertEquals(-20, testScoringRule.nodesScoreList.get(0).getScore(node1));
+        assertEquals(0, testScoringRule.nodesScoreList.get(0).getScore(node2));
+        
+        // build on node1
+        j.startBuild(node1Project, BUILD_TIMEOUT);
+        // build on node2
+        j.startBuild(node2Project, BUILD_TIMEOUT);
+        
+        // node1 busy 2
+        // node2 busy 1
+        testScoringRule.clear();
+        j.assertBuildStatusSuccess(testingProject.scheduleBuild2(0).get(BUILD_TIMEOUT, TimeUnit.SECONDS));
+        assertEquals(1, testScoringRule.nodesScoreList.size());
+        assertEquals(-40, testScoringRule.nodesScoreList.get(0).getScore(node1));
+        assertEquals(-20, testScoringRule.nodesScoreList.get(0).getScore(node2));
+        
+        for(Executor e: node1.toComputer().getExecutors())
+        {
+            if(e.isBusy())
+            {
+                e.interrupt(Result.ABORTED);
+            }
+        }
+        for(Executor e: node2.toComputer().getExecutors())
+        {
+            if(e.isBusy())
+            {
+                e.interrupt(Result.ABORTED);
+            }
+        }
+        
+        j.waitUntilNoActivityUpTo(BUILD_TIMEOUT * 1000);
+        
+        // node1 busy 0
+        // node2 busy 0
+        testScoringRule.clear();
+        j.assertBuildStatusSuccess(testingProject.scheduleBuild2(0).get(BUILD_TIMEOUT, TimeUnit.SECONDS));
+        assertEquals(1, testScoringRule.nodesScoreList.size());
+        assertEquals(0, testScoringRule.nodesScoreList.get(0).getScore(node1));
+        assertEquals(0, testScoringRule.nodesScoreList.get(0).getScore(node2));
+    }
+    
+    @Test
+    public void testBusyScoreForTaskset() throws Exception
+    {
+        Node node1 = j.createOnlineSlave("node", 4);
+        setScoringRule(new NodeLoadScoringRule(5, 0, -2));
+        
+        FreeStyleProject testingProject = j.createFreeStyleProject();
+        testingProject.setAssignedLabel(LabelExpression.parseExpression("node"));
+        DummySubTask taskOnSameNode = new DummySubTask("Task on same node", testingProject, 1000);
+        taskOnSameNode.setSameNodeConstraint(testingProject);
+        DummySubTask taskOnAnotherNode1 = new DummySubTask("Task on another node 1", testingProject, 1000);
+        taskOnAnotherNode1.setAssignedLabel(LabelExpression.parseExpression("node"));
+        DummySubTask taskOnAnotherNode2 = new DummySubTask("Task on another node 2", testingProject, 1000);
+        taskOnAnotherNode1.setAssignedLabel(LabelExpression.parseExpression("node"));
+        testingProject.addProperty(new TriggerOtherProjectProperty(taskOnSameNode, taskOnAnotherNode1, taskOnAnotherNode2));
+        
+        testScoringRule.clear();
+        
+        // build on node1
+        j.assertBuildStatusSuccess(testingProject.scheduleBuild2(0).get(BUILD_TIMEOUT, TimeUnit.SECONDS));
+        
+        assertEquals(3, testScoringRule.calledWorkChunkList.size());
+        // this is not promised, but seems always true.
+        assertEquals(testingProject, testScoringRule.calledWorkChunkList.get(0).get(0));
+        assertEquals(0, testScoringRule.nodesScoreList.get(0).getScore(node1));
+        assertEquals(-20, testScoringRule.nodesScoreList.get(1).getScore(node1));
+        assertEquals(-30, testScoringRule.nodesScoreList.get(2).getScore(node1));
     }
 }
