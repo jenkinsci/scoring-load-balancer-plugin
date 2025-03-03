50cc16e23a5c63ad961addd4585e2809fe1032fc
saville <saville@adobe.com>
2025-03-03 07:36:30 -0700

    Code review updates

--- src/test/java/jp/ikedam/jenkins/plugins/scoringloadbalancer/ConfigurationAsCodeTest.java
+++ src/test/java/jp/ikedam/jenkins/plugins/scoringloadbalancer/ConfigurationAsCodeTest.java
@@ -1,11 +1,13 @@
 package jp.ikedam.jenkins.plugins.scoringloadbalancer;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import hudson.ExtensionList;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
-import java.util.List;
 import jp.ikedam.jenkins.plugins.scoringloadbalancer.rules.BuildResultScoringRule;
 import jp.ikedam.jenkins.plugins.scoringloadbalancer.rules.NodeLoadScoringRule;
 import jp.ikedam.jenkins.plugins.scoringloadbalancer.rules.NodePreferenceScoringRule;
@@ -20,27 +22,26 @@ public class ConfigurationAsCodeTest {
     @Test
     @ConfiguredWithCode("configuration-as-code-test.yml")
     public void shouldSupportConfigurationAsCode() {
-        ExtensionList<ScoringLoadBalancer.DescriptorImpl> loadBalancerDescriptors =
-                rule.jenkins.getExtensionList(ScoringLoadBalancer.DescriptorImpl.class);
+        var loadBalancerDescriptors = rule.jenkins.getExtensionList(ScoringLoadBalancer.DescriptorImpl.class);
         assertEquals(1, loadBalancerDescriptors.size());
         assertFalse(loadBalancerDescriptors.get(0).isEnabled());
         assertTrue(loadBalancerDescriptors.get(0).isReportScoresEnabled());
 
-        List<ScoringRule> scoringRules = loadBalancerDescriptors.get(0).getScoringRuleList();
+        var scoringRules = loadBalancerDescriptors.get(0).getScoringRuleList();
 
-        assertTrue(scoringRules.get(0) instanceof NodeLoadScoringRule);
-        NodeLoadScoringRule nodeLoadScoringRule = (NodeLoadScoringRule) scoringRules.get(0);
+        assertThat(scoringRules.get(0), instanceOf(NodeLoadScoringRule.class));
+        var nodeLoadScoringRule = (NodeLoadScoringRule) scoringRules.get(0);
         assertEquals(20, nodeLoadScoringRule.getScale());
         assertEquals(2, nodeLoadScoringRule.getScoreForIdleExecutor());
         assertEquals(-2, nodeLoadScoringRule.getScoreForBusyExecutor());
 
-        assertTrue(scoringRules.get(1) instanceof NodePreferenceScoringRule);
-        NodePreferenceScoringRule nodePreferenceScoringRule = (NodePreferenceScoringRule) scoringRules.get(1);
+        assertThat(scoringRules.get(1), instanceOf(NodePreferenceScoringRule.class));
+        var nodePreferenceScoringRule = (NodePreferenceScoringRule) scoringRules.get(1);
         assertEquals(30, nodePreferenceScoringRule.getNodesPreferenceScale());
         assertEquals(35, nodePreferenceScoringRule.getProjectPreferenceScale());
 
-        assertTrue(scoringRules.get(2) instanceof BuildResultScoringRule);
-        BuildResultScoringRule buildResultScoringRule = (BuildResultScoringRule) scoringRules.get(2);
+        assertThat(scoringRules.get(2), instanceOf(BuildResultScoringRule.class));
+        var buildResultScoringRule = (BuildResultScoringRule) scoringRules.get(2);
         assertEquals(15, buildResultScoringRule.getNumberOfBuilds());
         assertEquals(40, buildResultScoringRule.getScale());
         assertEquals(-3, buildResultScoringRule.getScaleAdjustForOlder());
