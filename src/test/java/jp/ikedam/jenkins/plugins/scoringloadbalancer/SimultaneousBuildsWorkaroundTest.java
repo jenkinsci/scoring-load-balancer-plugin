5d30358178216d973bf53800be9c059371de22da
voruti <45130096+voruti@users.noreply.github.com>
2025-04-03 20:34:33 +0200

    Update license info

--- src/test/java/jp/ikedam/jenkins/plugins/scoringloadbalancer/SimultaneousBuildsWorkaroundTest.java
+++ src/test/java/jp/ikedam/jenkins/plugins/scoringloadbalancer/SimultaneousBuildsWorkaroundTest.java
@@ -2,6 +2,7 @@
  * The MIT License
  *
  * Copyright (c) 2013 IKEDA Yasuyuki
+ * Copyright (c) 2024 voruti
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
